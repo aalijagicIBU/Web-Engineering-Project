321fb3f55c6cf1925467872374f0c2d0a1fc6acd
aalijagicIBU <amina.alijagic@stu.ibu.edu.ba>
2019-04-18 02:03:09 +0200

    connected to mongodb

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 var express = require('express');
+var mongojs = require('mongojs')
+
 var app = express();
 
 app.use(express.static('public'));
 
 var port = process.env.PORT || 3000;
+var connectionString = "mongodb://library-user:library-user@library-app-shard-00-00-7kcfr.mongodb.net:27017,library-app-shard-00-01-7kcfr.mongodb.net:27017,library-app-shard-00-02-7kcfr.mongodb.net:27017/library-database?ssl=true&replicaSet=library-app-shard-0&authSource=admin&retryWrites=true";
+var db = mongojs(connectionString, ["books"])
 
 app.get('/library', function (req, res) {
   res.send('Welcome to your library!');
 });
+
+app.get('/books', function (req, res) {
+  books_list = [
+    {title:"Count of Monte Cristo", author:"Alexandar Dumas", publisher:"Vulkan"},
+    {title:"Harry Potter", author:"J.K.Rowling", publisher:"Algoritam"},
+    {title:"The Greatest Salesman in the World", author:"Og Mandino", publisher:"Laguna"}
+  ];
+
+  db.books.find(function (err, docs) {
+    res.send(docs);
+  })
+
+});
+
 app.listen(port, function () {
   console.log('Example app listening on port '+port+'!');
 });
--- public/js/BooksController.js
+++ public/js/BooksController.js
@@ -1,9 +1,7 @@
-function BooksController($scope) {
-    console.log("Test");
-
-    $scope.books_list = [
-        {title:"Count of Monte Cristo", author:"Alexandar Dumas", publisher:"Vulkan"},
-        {title:"Harry Potter", author:"J.K.Rowling", publisher:"Algoritam"},
-        {title:"The Greatest Salesman in the World", author:"Og Mandino", publisher:"Laguna"}
-    ]
+function BooksController($scope, $http) {
+    $http.get('/books').then(function(response){
+        $scope.books_list = response.data;
+    }, function(response){
+        $scope.books_list = response.data;
+    })
 }
